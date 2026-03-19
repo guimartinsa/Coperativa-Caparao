002c2a65360c1227edf078e10bb17de14a8baeae
guimartinsa <guilhermemaretinsyoutub13@gmail.com>
2026-03-18 22:02:13 -0300

    fix github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repository = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const isUserPageRepo = repository?.toLowerCase().endsWith(".github.io");
+const defaultBase = repository && !isUserPageRepo ? `/${repository}/` : "/";
+
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || defaultBase,
   plugins: [react()],
 });
